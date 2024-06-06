289d2c10a3ecad08967377c8691f005974fac64a
Clarence Jordan <clarencejordan087@gmail.com>
2024-06-05 23:21:57 -0500

    add: card builder and card data

--- scripts/dombuilders/card.js
+++ scripts/dombuilders/card.js
@@ -1,1 +1,32 @@
-const card1 = () => {};
+const card = (parent, obj) => {
+  const { shortForecast, windDirection, windSpeed, temperature } = obj;
+  const div = document.createElement("div");
+  div.className = "card w-96 glass";
+
+  const figure = document.createElement("figure");
+  const img = document.createElement("img");
+  img.src = "https://images.pexels.com/photos/301599/pexels-photo-301599.jpeg";
+  img.alt = "img";
+  figure.appendChild(img);
+  div.appendChild(figure);
+  const div2 = document.createElement("div");
+  div2.className = "card-body";
+  const h2 = document.createElement("h2");
+  h2.className = "card-title";
+  h2.innerText = "dallas";
+  div2.appendChild(h2);
+  const p = document.createElement("p");
+  const p1 = document.createElement("p");
+  const p2 = document.createElement("p");
+  p.innerHTML = temperature;
+  p1.innerHTML = `${windDirection}  ${windSpeed}`;
+  p2.innerHTML = shortForecast;
+  div2.appendChild(p);
+  div2.appendChild(p1);
+  div2.appendChild(p2);
+  div.appendChild(div2);
+
+  parent.appendChild(div);
+};
+
+export { card };
--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,31 @@
 import { cities } from "./data/citys.js";
+import { card } from "./dombuilders/card.js";
 import getPeriods from "./helpers/getPeriods.js";
+
 // dev only
 const city = cities[9].name;
 const latitude = cities[9].latitude;
 const longitude = cities[9].longitude;
+const card1Data = {
+  imgSrc: "https://images.pexels.com/photos/301599/pexels-photo-301599.jpeg",
+  altData: "sun",
+  h2Data: "Dallas",
+  pData: "temp: 32&deg",
+  p1Data: "winds: NNE 6MPH",
+  p2Data: "Partly cloudy",
+};
 
 const handleWeather = async () => {
+  const card1 = document.querySelector("#card1");
   const data = await getPeriods(latitude, longitude);
-  console.log(data);
+
+  const day = await data.filter((name) => name.name.split(" ").length < 2);
+
+  day.forEach((dayData) => {
+    console.log(dayData);
+    card(card1, dayData);
+  });
+  // card(card1, card1Data);
 };
 
 handleWeather();
